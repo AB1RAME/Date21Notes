950fba953c3764b6752f9b9c2034beaa49bf738d
AB1RAME <akumarathasan@spartaglobal.com>
2021-04-22 09:21:52 +0100

    keyvaluepairs

---
+++ keyvalue.py
@@ -0,0 +1,16 @@
+# student_1={
+#     'name':'Paula',
+#     'stream':'data',
+#     'list':['value1','value2','value3']
+# }
+# #print(student_1['list'][1])
+# student_1['name']='bob'
+# print(student_1.keys())
+
+
+#while
+# x=0
+# while x<10:
+#     print('this is true')
+#     x=x+1
+
